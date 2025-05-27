0cde3b077d6d081172371029f4e8004580eb58d1
Kurisani-web <kurisanim2@gmail.com>
2025-05-27 12:50:46 +0200

    Create Footer.tsx

---
+++ admin/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+// app/components/Footer.tsx
+"use client";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full text-center py-4 bg-gray-200 dark:bg-gray-800 dark:text-white">
+      <p>© 2025 CheFu Academy. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
