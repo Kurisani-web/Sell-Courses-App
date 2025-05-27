0e9ef26c1580d302b70679b9f1bbf2582be324cb
Kurisani-web <kurisanim2@gmail.com>
2025-05-27 12:40:30 +0200

    Create CheckOutForm.tsx

---
+++ admin/app/components/Payment/CheckOutForm.tsx
@@ -0,0 +1,13 @@
+// app/components/Payment/CheckOutForm.tsx
+"use client";
+import React from "react";
+
+const CheckOutForm = () => {
+  return (
+    <div>
+      <p>Checkout form placeholder</p>
+    </div>
+  );
+};
+
+export default CheckOutForm;
